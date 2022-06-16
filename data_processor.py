40426626d58183c852f1fac51279fc34762b0c24
LabPC <kaneda@photon.t.u-tokyo.ac.jp>
2022-06-16 19:56:02 +0900

    add cosmic ray removal

--- data_processor.py
+++ data_processor.py
@@ -5,13 +5,39 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 from scipy.signal import find_peaks
+from scipy.stats import multivariate_normal
 from sklearn.linear_model import LinearRegression
 
 
+def modified_z_score(intensity):
+    median_int = np.median(intensity)
+    mad_int = np.median([np.abs(intensity - median_int)])
+    modified_z_scores = 0.6745 * (intensity - median_int) / mad_int
+    return modified_z_scores
+
+
+def fixed_z(y, m):
+    threshold = 7  # 閾値
+    spikes = abs(np.array(modified_z_score(np.diff(y)))) > threshold
+    y_out = y.copy()
+    for i in np.arange(len(spikes)):
+        if spikes[i]:  # If we have a spike in position i
+            w = np.arange(i - m, i + 1 + m)  # スパイク周りの2 m + 1個のデータを取り出す
+            w = w[0 <= w]  # 範囲を超えないようトリミング
+            w = w[w < 1023]
+            w2 = w[spikes[w] == False]  # スパイクでない値を抽出し，
+            if len(w2) > 0:
+                y_out[i] = np.mean(y[w2])  # 平均を計算し補完
+
+    return y_out
+
+
 class DataProcessor:
-    def __init__(self, path, center_wl=630):
+    def __init__(self, path, center_wl: float = 630):
         self.path = path
         self.df = None
+        self.df_removed_cosmic_ray = None
+        self.num_data = 0
         self.center_wl = center_wl
         self.wl_data = np.linspace(self.center_wl - 65, self.center_wl + 65, 1024)
         self.peaks_found_wl = None
@@ -31,13 +57,23 @@ class DataProcessor:
     def load_data(self):
         filenames = glob.glob(self.path + '/*.asc')
 
+        def sort_key(fn):
+            fn_sep = fn.split(os.path.sep)[-1]
+            if 'calibration' in fn_sep:
+                return -1
+            fn_sep = fn_sep.strip('acquisition')
+            fn_sep = fn_sep[:fn_sep.find('of')]
+            return int(fn_sep) + 1
+        filenames.sort(key=sort_key)
+
         df_list = []
         for filename in filenames:
             df = pd.read_csv(filename, header=None).T
             df.index = [filename.split(os.path.sep)[-1]]
             df_list.append(df)
+            if 'calibration' not in df.index[0]:
+                self.num_data += 1
         self.df = pd.concat(df_list, axis=0)
-        self.df = self.df.sort_index()
 
     def quick_calibration(self, show=False):
         data_calibration = self.df.filter(like='calibration', axis=0)
@@ -77,7 +113,26 @@ class DataProcessor:
         lr.fit(peaks_found_wl, peaks)
         self.wl_data_calibrated = lr.predict(wl_data)
 
-    def draw(self):
+    def remove_cosmic_ray(self, times: int = 0):
+        df_list = []
+        for name, data in self.df.iterrows():
+            if 'calibration' in name:
+                continue
+            z = data.values
+            for i in range(times):
+                z = fixed_z(z, 3)
+            df_new = pd.DataFrame(data=z).T
+            df_new.index = [name]
+            df_list.append(df_new)
+        self.df_removed_cosmic_ray = pd.concat(df_list, axis=0)
+
+    def draw(self, cosmic_ray_removal=False, surface=True):
+        df = self.df
+        if cosmic_ray_removal:
+            if self.df_removed_cosmic_ray is None:
+                self.remove_cosmic_ray(times=1)
+            df = self.df_removed_cosmic_ray
+
         fig = plt.figure(figsize=(12, 8))
         ax = fig.add_subplot(projection='3d')
         if self.wl_data_calibrated is None:
@@ -85,23 +140,32 @@ class DataProcessor:
         else:
             x = self.wl_data_calibrated
 
-        for i, (filename, data) in enumerate(self.df.iterrows()):
+        y_surface = np.arange(0, self.num_data)
+        z_surface = []
+        for i, (filename, data) in enumerate(df.iterrows()):
             if 'calibration' in filename:
                 continue
-
             y = [i] * 1024
             z = data.values
-            ax.plot(x, y, z, color=cm.gist_earth(i / self.df.shape[0]))
+            if surface:
+                z_surface.append(z)
+            else:
+                ax.plot(x, y, z, color=cm.gist_earth(i / self.num_data))
 
-        ax.set_zlim(0, 40000)
+        if surface:
+            x_mesh, y_mesh = np.meshgrid(x, y_surface)
+            z_surface = np.array(z_surface)
+            ax.plot_surface(x_mesh, y_mesh, z_surface, cmap='gist_earth', linewidth=1)
+
         plt.show()
 
 
 def main():
     path = r"G:\共有ドライブ\Laboratory\Individuals\kaneda\Data_M1\220616\SCANT"
     dp = DataProcessor(path=path)
-    dp.quick_calibration(show=True)
-    dp.draw()
+    dp.quick_calibration(show=False)
+    dp.remove_cosmic_ray(times=3)
+    dp.draw(cosmic_ray_removal=False, surface=True)
 
 
 if __name__ == '__main__':
